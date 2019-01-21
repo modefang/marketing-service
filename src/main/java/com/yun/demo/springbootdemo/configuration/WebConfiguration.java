3676d95c26b8c35ddeea52495219105cd0646e86
modefang <modefang936@gmail.com>
2019-01-21 16:30:48 +0800

    add limit ip request

--- src/main/java/com/yun/demo/springbootdemo/configuration/WebConfiguration.java
+++ src/main/java/com/yun/demo/springbootdemo/configuration/WebConfiguration.java
@@ -1,19 +1,17 @@
 package com.yun.demo.springbootdemo.configuration;
 
 import com.yun.demo.springbootdemo.interceptor.LimitInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    // 使用@bean创建拦截器类，才能在拦截器里使用@Resource或者@Autowired
-    @Bean
-    public LimitInterceptor getLimitInterceptor() {
-        return new LimitInterceptor();
-    }
+    @Resource
+    private LimitInterceptor limitInterceptor;
 
     /**
      * 自定义拦截器，添加拦截路径和排除拦截路径
@@ -22,7 +20,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 限制每个IP访问接口的次数，拦截所有接口
-        registry.addInterceptor(getLimitInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(this.limitInterceptor).addPathPatterns("/**");
     }
 
 }
