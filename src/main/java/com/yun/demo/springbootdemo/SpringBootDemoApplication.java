27ea13b255f10b2096271df5805178c3122b23a7
modefang <modefang936@gmail.com>
2019-03-13 14:52:49 +0800

    delete spring security

--- src/main/java/com/yun/demo/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/yun/demo/springbootdemo/SpringBootDemoApplication.java
@@ -2,9 +2,7 @@ package com.yun.demo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
-@EnableOAuth2Sso
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
